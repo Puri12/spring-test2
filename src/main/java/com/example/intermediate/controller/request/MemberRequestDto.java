eff75f4cd9e9ded93a1512a8e085b53823d42d09
Puri12 <tkdqja9812@gmail.com>
2022-10-13 12:46:48 +0900

    WebMvcConfigurer complate

--- src/main/java/com/example/intermediate/controller/request/MemberRequestDto.java
+++ src/main/java/com/example/intermediate/controller/request/MemberRequestDto.java
@@ -14,14 +14,14 @@ import lombok.NonNull;
 @AllArgsConstructor
 public class MemberRequestDto {
 
-  @NotBlank(message = "nickname은 공백일 수 없습니다.")
-  @Size(min = 4,max = 12, message = "nickname은 4~12 개의 영문자만 허용합니다.")
+  @NotBlank(message = "{nickname.notnull}")
+  @Size(min = 4,max = 12, message = "{nickname.size}")
   @Pattern(regexp = "^[a-z]*$")
   private String nickname;
 
-  @NotBlank(message = "패스워드는 공백일 수 없습니다.")
-  @Size(min = 8,max = 20, message = "패스워드는 8~20 개의 문자만 허용합니다.")
-  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,20}$", message = "패스워드는 무조건 영문, 숫자, 특수문자를 각각 1글자 이상 포함해야 합니다.")
+  @NotBlank(message = "{password.notnull}")
+  @Size(min = 8,max = 20, message = "{password.size}")
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,20}$", message = "{password.pattern}")
   private String password;
 
   @NotBlank
